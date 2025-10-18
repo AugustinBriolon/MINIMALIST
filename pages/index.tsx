e38950fe41e8b9dbf903dc8566733320d89b3e53
Augustin Briolon <augustin.briolon@gmail.com>
2025-10-18 10:55:53 +0200

    try fix problem

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Footer from '@/components/layout/Footer';
 import Hero from '@/components/layout/Hero';
 import Ingredients from '@/components/layout/Ingredients';
 import Performance from '@/components/layout/Performance';
-import WhatInside from '@/components/layout/WhatInside';
+import WhatInside from '@/components/layout/WhatInsid';
 import WhatSpecial from '@/components/layout/WhatSpecial';
 import WhereToBuy from '@/components/layout/WhereToBuy';
 import WhoWeAre from '@/components/layout/WhoWeAre';
