be2ad18ea97b3903c5786311950431db98e84d3d
Augustin Briolon <augustin.briolon@gmail.com>
2025-10-22 14:22:02 +0200

    remove unused import

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,4 @@
-import { useRef } from 'react';
 import { Logo } from '../ui/Icons';
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
 
 export default function Footer() {
   return (
